40b8653f9176adf0d3f5aed9034cff4792615a8c
Jonas Andersson <jonananas@gmail.com>
2013-06-16 22:43:44 +0200

    Added requestAnimFrame and canvas looparound for sprite

--- canvas/src/main/webapp/canvas.js
+++ canvas/src/main/webapp/canvas.js
@@ -1,3 +1,12 @@
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
 function Frame() {
 	this.canvas = document.createElement("canvas");
 	this.ctx = this.canvas.getContext("2d");
@@ -8,9 +17,9 @@ function Frame() {
 	this.clear = function() {
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	};
-	
+
 	this.render = function(sprite) {
-		sprite.render(this.ctx);
+		sprite.render(this.ctx, this);
 	};
 }
 
@@ -19,8 +28,20 @@ function Circle() {
 	this.y = 100;
 	this.radius = 100;
 	this.color = "red";
+	this.xMovement = 0;
+	this.yMovement = 0;
 
-	this.render = function(ctx) {
+	this.render = function(ctx, frame) {
+		this.x += this.xMovement;
+		this.y += this.yMovement;
+		if (this.y > frame.canvas.height + this.radius)
+			this.y = 0 - this.radius;
+		if (this.y < 0 - this.radius)
+			this.y = frame.canvas.height + this.radius;
+		if (this.x > frame.canvas.width + this.radius)
+			this.x = 0 - this.radius;
+		if (this.x < 0 - this.radius)
+			this.x = frame.canvas.width + this.radius;
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
 		ctx.fillStyle = this.color;
@@ -28,17 +49,22 @@ function Circle() {
 	};
 
 	this.moveRight = function(pixels) {
-		this.x += pixels;
+		this.xMovement += pixels;
 	};
 
 	this.moveDown = function(pixels) {
-		this.y += pixels;
+		this.yMovement += pixels;
 	};
 }
 
 frame = new Frame();
 circle = new Circle();
-frame.render(circle);
+
+(function animloop(time) {
+	requestAnimFrame(animloop);
+	frame.clear();
+	frame.render(circle);
+})();
 
 $("body").keydown(function(event) {
 	switch (event.keyCode) {
@@ -57,6 +83,4 @@ $("body").keydown(function(event) {
 	default:
 		console.log("pressed " + event.keyCode);
 	}
-	frame.clear();
-	frame.render(circle);
 });
